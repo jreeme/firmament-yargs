d78375efeb9acbb82a301567633bac51455051c1
John Reeme <john.reeme@soteradefense.com>
2016-04-12 13:49:28 -0500

    Sixth commit to test using npm to get module from git

--- js/implementations/commandLineImpl.d.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from "../interfaces/command";
-import { CommandLine } from "../interfaces/command-line";
-export declare class CommandLineImpl implements CommandLine {
-    private cli;
-    private app;
-    addCommand(cmd: Command): void;
-    exec(): void;
-}
