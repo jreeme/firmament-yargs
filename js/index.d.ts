ae48ceb8576e41d08270a9559679ea9e41ad6013
John Reeme <john.reeme@soteradefense.com>
2016-04-12 13:41:00 -0500

    Fourth commit to test using npm to get module from git

--- js/index.d.ts
+++ js/index.d.ts
@@ -1,1 +1,4 @@
 export * from './interfaces/command';
+export * from './interfaces/command-line';
+export * from './implementations/command-impl';
+export * from './implementations/command-line-impl';
--- ts/index.ts
+++ ts/index.ts
@@ -1,2 +1,5 @@
 // Sample exports from other modules
 export * from './interfaces/command';
+export * from './interfaces/command-line';
+export * from './implementations/command-impl';
+export * from './implementations/command-line-impl';
