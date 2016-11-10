4a1938b98fa8548305869a124f6a5f968d39d4a2
John Reeme <john.reeme@soteradefense.com>
2016-11-10 10:45:05 -0600

    -> v.0.2.9

--- js/index.d.ts
+++ js/index.d.ts
@@ -4,5 +4,6 @@ export * from './interfaces/command-util';
 export * from './interfaces/spawn';
 export * from './interfaces/command-line';
 export * from './interfaces/progress-bar';
+export * from './interfaces/positive';
 import kernel from "./inversify.config";
 export { kernel };
--- ts/index.ts
+++ ts/index.ts
@@ -5,6 +5,7 @@ export * from './interfaces/command-util';
 export * from './interfaces/spawn';
 export * from './interfaces/command-line';
 export * from './interfaces/progress-bar';
+export * from './interfaces/positive';
 import kernel from "./inversify.config";
 export {kernel};
 
